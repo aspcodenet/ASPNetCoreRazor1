62826ecb3adafae84ca8da741e851eea056b1ad9
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-07-05 14:53:06 +0200

    Add project files.

---
+++ ASPNetCoreRazor/Pages/Privacy.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreRazor.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+
+        public string WeekdayInSwedish { get; set; }
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            //Hämta data från databasen
+
+            var svenska = new CultureInfo("sv-SE");
+            WeekdayInSwedish = DateTime.Now.ToString("dddd", svenska);
+
+        }
+    }
+}
