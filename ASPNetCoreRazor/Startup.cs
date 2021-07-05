76340d42714f38d39e65db17f26dc038b5796fdd
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-07-05 17:07:05 +0200

    service

--- ASPNetCoreRazor/Startup.cs
+++ ASPNetCoreRazor/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASPNetCoreRazor.Services;
 
 namespace ASPNetCoreRazor
 {
@@ -23,6 +24,7 @@ namespace ASPNetCoreRazor
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IKrisInfoService, KrisInfoService>();
             services.AddRazorPages();
         }
 
