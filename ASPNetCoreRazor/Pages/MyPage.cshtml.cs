caa40da70ce35703b66a4f88212497fa1f341db7
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-07-05 15:03:54 +0200

    lab2

---
+++ ASPNetCoreRazor/Pages/MyPage.cshtml.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPNetCoreRazor.Pages
+{
+    public class MyPageModel : PageModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        
+        public void OnGet()
+        {
+            Name = "Kalle";
+            Age = 27;
+        }
+    }
+}
